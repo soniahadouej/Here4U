5b40e0dfbfc612fc22d41599f0ee9bd4b04851f7
Sandouda <Sandouda@DESKTOP-J0EE2C6>
2021-04-16 02:46:10 +0100

    Authentification

--- Pidev-G3-(1)/src/main/java/tn/g3/spring/PidevG3Application.java
+++ Pidev-G3-(1)/src/main/java/tn/g3/spring/PidevG3Application.java
@@ -2,8 +2,16 @@ package tn.g3.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import tn.g3.spring.PidevG3Application;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		PidevG3Application.class,
+        Jsr310JpaConverters.class
+})
 public class PidevG3Application {
 
 	public static void main(String[] args) {
